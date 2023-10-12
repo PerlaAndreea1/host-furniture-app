a6bb3f8a7d24be3c262859c89b40b2c402db2b77
Perla Railescu <perla.railescu@fabelx.com>
2023-10-12 16:32:23 +0000

    add another dropdown for multiple furniture options

--- src/components/CustomDesign.vue
+++ src/components/CustomDesign.vue
@@ -1,7 +1,9 @@
 <template lang="pug">
 .room
-  el-select(v-model="selectedImage" placeholder='Select Furniture')
-    el-option(v-for="(option, index) in availableImages" :key="index" :label='option.name' :value="index") {{ option.name }}
+  el-select(v-model="selectedImage" placeholder='Select Furniture' @change="updateOptions")
+    el-option(v-for="(option, index) in availableImages" :key="index" :label='option.name' :value="index" ) {{ option.name }}
+  el-select(v-model="selectedOption", placeholder="Select Option")
+    el-option(v-for="(option, index) in availableOptions" :key="index" :label="option.name" :value="index") {{ option.name }}
   el-button(@click="addImageToScene") Add Selected Image
   el-button(@click="deleteImage") Delete Selected Image
   // Render the room background
@@ -37,55 +39,27 @@ export default {
 				images: [],
           availableImages:[
           {
-            name: 'Hanging Lamp',
-						src: 'https://www.pngmart.com/files/16/Electric-Hanging-Lamp-Transparent-PNG.png',
-						top: 30,
-						left: 700,
-						height: 300,
-						width: 300,
+            name: 'Hanging Lamp'
 					},
           {
             name: 'Picture',
-						src: 'https://www.pngmart.com/files/22/Landscape-Painting-Art-PNG-HD.png',
-						top: 250,
-						left: 900,
-						height: 250,
-						width: 250,
 					},
 					{
-            name: 'Sofa',
-						src: 'https://www.pngmart.com/files/7/Sleeper-Sofa-Transparent-Images-PNG.png',
-						top: 500,
-						left: 400,
-						height: 300,
-						width: 600,
+            name: 'Sofa'
 					},
 					{
             name: 'Table',
-						src: 'https://www.pngmart.com/files/7/Drum-Table-PNG-Image.png',
-						top: 550,
-						left: 1200,
-						height: 300,
-						width: 300,
 					},
 					{
             name: 'Armchair',
-						src: 'https://www.pngmart.com/files/8/Armchair-PNG-Image.png',
-						top: 580,
-						left: 0,
-						height: 400,
-						width: 500,
 					},
 					{
             name: 'Plant',
-						src: 'https://www.pngmart.com/files/13/Aloe-Vera-Potted-PNG-Pic.png',
-						top: 580,
-						left: 1000,
-						height: 200,
-						width: 150,
 					},
 				],
+        availableOptions: [],
         selectedImage: '',
+        selectedOption: '',
         isDraggingBackground: false,
         backgroundStartX: 0,
         backgroundStartY: 0,
@@ -96,31 +70,239 @@ export default {
     },
 
   methods: {
+    updateOptions() {
+      if (this.selectedImage !== '') {
+        const selectedFurniture = this.availableImages[this.selectedImage];
+        // Define options based on the selected furniture
+        switch (selectedFurniture.name) {
+          case 'Hanging Lamp':
+            this.availableOptions = [
+              {
+                name: 'Modern Lamp',
+                src: 'https://www.pngmart.com/files/7/Hanging-Light-PNG-HD.png',
+                top: 40,
+                left: 750,
+                height: 200,
+                width: 200,
+              },
+              {
+                name: 'Vintage Lamp',
+                src: 'https://www.pngmart.com/files/7/Hanging-Light-Transparent-Background.png',
+                top: 20,
+                left: 700,
+                height: 300,
+                width: 300,
+              },
+              {
+                name: 'Antique Lamp',
+                src: 'https://www.pngmart.com/files/7/Chandelier-PNG-Pic.png',
+                top: 0,
+                left: 700,
+                height: 300,
+                width: 300,
+              },
+            ];
+            break;
+          case 'Sofa':
+            this.availableOptions = [
+              {
+                name: 'Yellow Sofa',
+                src: 'https://www.pngmart.com/files/7/Yellow-Sofa-PNG-Photos.png',
+                top: 400,
+                left: 400,
+                height: 500,
+                width: 600,
+              },
+              {
+                name: 'Blue Sofa',
+                src: 'https://www.pngmart.com/files/7/Sofa-Bed-Transparent-Background.png',
+                top: 400,
+                left: 400,
+                height: 550,
+                width: 600,
+              },
+              {
+                name: 'Cream Sofa',
+                src: 'https://www.pngmart.com/files/7/Sleeper-Sofa-Transparent-Images-PNG.png',
+                top: 500,
+                left: 400,
+                height: 300,
+                width: 600,
+              },
+            ];
+            break;
+          case 'Picture':
+            this.availableOptions = [
+              {
+                name: 'Landscape',
+                src: 'https://www.pngmart.com/files/22/Landscape-Painting-Art-PNG-HD.png',
+                top: 250,
+                left: 900,
+                height: 250,
+                width: 250,
+              },
+              {
+                name: 'Portrait',
+                src: 'https://www.pngmart.com/files/22/Portrait-Art-PNG.png',
+                top: 250,
+                left: 900,
+                height: 250,
+                width: 200,
+              },
+              {
+                name: 'Modern Art',
+                src: 'https://www.pngmart.com/files/11/Modern-Art-Transparent-Background.png',
+                top: 250,
+                left: 900,
+                height: 250,
+                width: 250,
+              },
+            ];
+            break;
+          case 'Table':
+            this.availableOptions = [
+              {
+                name: 'Modern Table',
+                src: 'https://www.pngmart.com/files/7/Drum-Table-PNG-Image.png',
+                top: 550,
+                left: 1200,
+                height: 300,
+                width: 300,
+              },
+              {
+                name: 'Metal Table',
+                src: 'https://www.pngmart.com/files/7/Drum-Table-PNG-HD.png',
+                top: 550,
+                left: 1200,
+                height: 300,
+                width: 300,
+              },
+              {
+                name: 'Wooden Table',
+                src: 'https://www.pngmart.com/files/7/Drum-Table-PNG-Background-Image.png',
+                top: 600,
+                left: 1100,
+                height: 200,
+                width: 300,
+              },
+            ];
+            break;
+          case 'Armchair':
+            this.availableOptions = [
+              {
+                name: 'Blue Armchair',
+                src: 'https://www.pngmart.com/files/8/Armchair-PNG-Image.png',
+                top: 580,
+                left: 0,
+                height: 400,
+                width: 500,
+              },
+              {
+                name: 'Brown Armchair',
+                src: 'https://www.pngmart.com/files/8/Armchair-PNG-File.png',
+                top: 580,
+                left: 0,
+                height: 400,
+                width: 400,
+              },
+              {
+                name: 'Gray Armchair',
+                src: 'https://www.pngmart.com/files/3/Armchair-PNG-Image.png',
+                top: 580,
+                left: 0,
+                height: 400,
+                width: 500,
+              },
+            ];
+            break;
+          case 'Plant':
+            this.availableOptions = [
+              {
+                name: 'Aloe Vera',
+                src: 'https://www.pngmart.com/files/13/Aloe-Vera-Potted-PNG-Pic.png',
+                top: 580,
+                left: 1000,
+                height: 200,
+                width: 150,
+              },
+              {
+                name: 'Pothos',
+                src: 'https://www.pngmart.com/files/21/Aesthetic-Plant-PNG-Free-Download.png',
+                top: 580,
+                left: 1000,
+                height: 200,
+                width: 150,
+              },
+              {
+                name: 'Palm Tree',
+                src: 'https://www.pngmart.com/files/22/Plant-PNG.png',
+                top: 400,
+                left: 1000,
+                height: 400,
+                width: 200,
+              },
+            ];
+            break;
+          default:
+            this.availableOptions = []; // Reset options
+            break;
+        }
+      } else {
+        this.availableOptions = []; // Reset options if no furniture selected
+      }
+    },
     updateImagePosition(index, newPosition) {
       // Update the position of the image in the data array
       this.images[index].top = newPosition.top;
       this.images[index].left = newPosition.left;
     },
     addImageToScene() {
-      if (this.selectedImage !== '') {
-        const selectedImage = this.availableImages[this.selectedImage];
-        const isDuplicate = this.images.some((image) => {
-          return (
-            image.src === selectedImage.src &&
-            image.top === selectedImage.top &&
-            image.left === selectedImage.left &&
-            image.height === selectedImage.height &&
-            image.width === selectedImage.width
-          );
+      if (this.selectedImage !== '' && this.selectedOption !== '') {
+        const selectedFurniture = this.availableImages[this.selectedImage];
+        const selectedOption = this.availableOptions[this.selectedOption];
+
+        // Check if an image with the same furniture type already exists in the scene
+        const existingImageIndex = this.images.findIndex((image) => {
+          return image.name.startsWith(selectedFurniture.name);
         });
-        if (!isDuplicate) {
-          const newImage = {
-            ...selectedImage, 
-          };
-          this.images.push(newImage);
+
+        if (existingImageIndex !== -1) {
+          // If an image of the same furniture type exists, replace it
+          this.images.splice(existingImageIndex, 1);
         }
+
+        const newImage = {
+          name: `${selectedFurniture.name} - ${selectedOption.name}`,
+          src: selectedOption.src,
+          top: selectedOption.top,
+          left: selectedOption.left,
+          height: selectedOption.height,
+          width: selectedOption.width,
+        };
+
+        this.images.push(newImage);
       }
     },
+    // addImageToScene() {
+    //   if (this.selectedImage !== '') {
+    //     const selectedImage = this.availableImages[this.selectedImage];
+    //     const isDuplicate = this.images.some((image) => {
+    //       return (
+    //         image.src === selectedImage.src &&
+    //         image.top === selectedImage.top &&
+    //         image.left === selectedImage.left &&
+    //         image.height === selectedImage.height &&
+    //         image.width === selectedImage.width
+    //       );
+    //     });
+    //     if (!isDuplicate) {
+    //       const newImage = {
+    //         ...selectedImage, 
+    //       };
+    //       this.images.push(newImage);
+    //     }
+    //   }
+    // },
     deleteImage() {
       if (this.selectedImage !== '') {
         const index = this.images.findIndex((image) => {
